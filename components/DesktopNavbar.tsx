7d184df9530550640c266441e0bd8c4014b1a57a
AmitBiswas1402 <amit.142biswas@gmail.com>
2026-01-01 21:32:21 +0530

    Add Prisma integration and user synchronization functionality

--- components/DesktopNavbar.tsx
+++ components/DesktopNavbar.tsx
@@ -18,23 +18,24 @@ const DesktopNavbar = async () => {
 
       {user && (
         <>
-        <Button variant="ghost" className="flex items-center gap-2" asChild>
-          <Link href="/notifications">
-            <BellIcon className="w-4 h-4" />
-            <span className="hidden lg:inline">Notifications</span>
-          </Link>
-        </Button>
-        <Button variant="ghost" className="flex items-center gap-2" asChild>
-          <Link
-            href={`/profile/${
-              user.username ?? user.emailAddresses[0]?.emailAddress.split("@")[0]
-            }`}
-          >
-            <UserIcon className="w-4 h-4" />
-            <span className="hidden lg:inline">Profile</span>
-          </Link>
-        </Button>
-      </>
+          <Button variant="ghost" className="flex items-center gap-2" asChild>
+            <Link href="/notifications">
+              <BellIcon className="w-4 h-4" />
+              <span className="hidden lg:inline">Notifications</span>
+            </Link>
+          </Button>
+          <Button variant="ghost" className="flex items-center gap-2" asChild>
+            <Link
+              href={`/profile/${
+                user.username ??
+                user.emailAddresses[0]?.emailAddress.split("@")[0]
+              }`}
+            >
+              <UserIcon className="w-4 h-4" />
+              <span className="hidden lg:inline">Profile</span>
+            </Link>
+          </Button>
+        </>
       )}
 
       <AuthNavbar />
